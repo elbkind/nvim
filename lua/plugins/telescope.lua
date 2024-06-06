9accf0ab605903141b3c559e4cbf549a569ac5a9
Mark Vollmann <mark.vollmann@sinnerschrader.com>
2024-06-06 14:27:06 +0200

    optimized telescope config
    using existing funtionality

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,31 +1,33 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("telescope").setup({
-      defaults = {
-        selection_caret = "󱞩 ",
-        prompt_prefix = " 🔍 ",
-        path_display = {
-          "filename_first",
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          selection_caret = "󱞩 ",
+          prompt_prefix = " 🔍 ",
+          path_display = {
+            "filename_first",
+          },
         },
-      },
-      pickers = {
-        live_grep = {
-          additional_args = function()
-            return { "--hidden" }
-          end,
+        pickers = {
+          live_grep = {
+            additional_args = function()
+              return { "--hidden" }
+            end,
+          },
+          grep_string = {
+            additional_args = function()
+              return { "--hidden" }
+            end,
+          },
         },
-        grep_string = {
-          additional_args = function()
-            return { "--hidden" }
-          end,
-        },
-      },
-    })
-  end,
-  keys = {
-    { "<leader>/", LazyVim.telescope("live_grep", { cwd = false }), desc = "Grep (CWD Dir)" },
-    { "<leader><space>", LazyVim.telescope("files", { cwd = false }), desc = "Find Files (CWD Dir)" },
+      })
+    end,
+    keys = {
+      { "<leader>/", LazyVim.telescope("live_grep", { cwd = false }), desc = "Grep (CWD Dir)" },
+      { "<leader><space>", LazyVim.telescope("files", { cwd = false }), desc = "Find Files (CWD Dir)" },
+    },
   },
 }
