56ef274a4dd0c8813b0cace93e22d8cbeb888ec2
Mark Vollmann <mark.vollmann@sinnerschrader.com>
2024-11-01 20:40:30 +0100

    use github dark colorblind

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+--require("newpaper").setup({})
+require("github-theme").setup({})
+vim.cmd("colorscheme github_dark_colorblind")
--- lua/plugins/githhub-colorscheme.lua
+++ lua/plugins/githhub-colorscheme.lua
@@ -1,9 +1,16 @@
 return {
   { "projekt0n/github-nvim-theme" },
+  { "yorik1984/newpaper.nvim", opts = {
+
+    italic_strings = false,
+  } },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_dark_colorblind",
+      --  colorscheme = "github_dark_colorblind",
+      --colorscheme = "github_light_colorblind",
+      --colorscheme = "newpaper",
+      --colorscheme = "tokyonight-day",
     },
   },
 }
