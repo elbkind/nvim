47ba3a2d5025d9010766e065a9c83752a18f5067
Mark Vollmann <mark.vollmann@accenture.com>
2024-12-23 19:58:18 +0100

    fix fzf
    used setup and killed the default setup
    now we just add a config option, to be
    merged into the default configuration

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,12 +2,9 @@ return {
   "ibhagwan/fzf-lua",
   -- optional for icon support
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("fzf-lua").setup({
-      defaults = {
-        formatter = "path.filename_first",
-        --       path_shorten = true,
-      },
-    })
-  end,
+  opts = {
+    defaults = {
+      formatter = "path.filename_first",
+    },
+  },
 }
